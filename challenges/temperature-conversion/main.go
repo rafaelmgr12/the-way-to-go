c80701701b58b7ecaa46877098d3869deeddf3cf
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-11-23 10:55:13 -0300

    add challenge

---
+++ challenges/temperature-conversion/main.go
@@ -0,0 +1,21 @@
+/*
+Challenge: Temperature Conversion
+Implement function toFahrenheit(t Celsius)to convert from Celsius to Fahrenheit. Types of temperatures are float32 which are aliased to Celsius and Fahrenheit for you.
+
+Temperature in ˚F = (Temp in °C * 9/5) + 32
+(Tempin°C∗9/5)+32
+
+*/
+package main
+
+// aliasing type
+type Celsius float32
+type Fahrenheit float32
+
+// Function to convert celsius to fahrenheit
+func toFahrenheit(t Celsius) Fahrenheit {
+
+	var temp Fahrenheit
+	return temp
+
+}
